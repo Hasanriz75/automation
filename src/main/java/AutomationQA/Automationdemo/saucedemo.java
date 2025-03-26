acc5757f1568fc6ba4140e8ea861f8974ad727e2
Hasanriz75 <hasanriz75@gmail.com>
2025-03-26 21:00:09 +0530

    Update saucedemo.java-assert method

--- src/main/java/AutomationQA/Automationdemo/saucedemo.java
+++ src/main/java/AutomationQA/Automationdemo/saucedemo.java
@@ -1,19 +1,12 @@
-package AutomationQA.Automationdemo;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class saucedemo {
-
+	@Test
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
 driver.get("https://www.saucedemo.com/inventory.html");
 		
-		System.out.println(driver.getTitle());
+		System.out.println("Verfying Scenario");
 		
 		driver.manage().window().maximize();
 	    driver.findElement(By.id("user-name")).sendKeys("HASAN");
@@ -28,23 +21,22 @@ driver.get("https://www.saucedemo.com/inventory.html");
 	    driver.findElement(By.id("password")).sendKeys("secret_sauce");
 	    Thread.sleep(2000);
 	    driver.findElement(By.id("login-button")).click();
-	    System.out.println(driver.getTitle());
+	    String actualtitle = driver.getTitle();
+	    String exptitle = "Swag Labs";
+	    Assert.assertEquals(actualtitle,exptitle);
+	 
 	    driver.findElement(By.xpath("(//button[@class='btn btn_primary btn_small btn_inventory '])[1]")).click();
 	   String c = driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).getText();
-	    if (c.equals("1")) {
-	    	System.out.println("Qty is 1");
-	    }
+	   Assert.assertTrue(c.equals("1"));    
+	    
 	    driver.findElement(By.xpath("(//button[@class='btn btn_primary btn_small btn_inventory '])[2]")).click();
 	    String d = driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).getText();
-	    if (d.equals("2")) {
-	    	System.out.println("Qty is 2");
-	    }
+	    Assert.assertTrue(d.equals("2"));    
+	   
 	   driver.findElement(By.id("remove-sauce-labs-bolt-t-shirt")).click();
 	   String e = driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).getText();
-	    if (e.equals("1")) {
-	    	System.out.println("Qty is 1");
-	    }
-	    driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();
+	   Assert.assertTrue(e.equals("1"));    
+	       
 	    driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
 	    driver.getCurrentUrl().contains("cart");
 	    driver.findElement(By.id("continue-shopping")).click();
@@ -60,8 +52,10 @@ driver.get("https://www.saucedemo.com/inventory.html");
 	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//*[text()='Sauce Labs Backpack']"));
 	    driver.findElement(By.id("finish")).click();
-	    driver.findElement(By.xpath("//*[text()='Thank you for your order!']"));
+	    String f = driver.findElement(By.xpath("//h2[@class='complete-header']")).getText();
+	    Assert.assertEquals(f,"Thank you for your order!");
 	    System.out.println("Scenario verified");
 	    driver.quit();
 	}
+	
 }
